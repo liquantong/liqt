030e33d60f3dc501247811a3f48bab34fadd328d
51108 <1301754139@qq.com>
2019-11-11 14:27:56 +0800

    oo

--- testFactory/src/com/lqt/model/QiXingNife.java
+++ testFactory/src/com/lqt/model/QiXingNife.java
@@ -4,5 +4,7 @@ public class QiXingNife extends NifeModel{
 	public QiXingNife(){
 		this.setNife("七星宝刀");
 		System.out.println("hahah");
+		System.out.println("hahah");
+		System.out.println("hahah");
 	}
 }
